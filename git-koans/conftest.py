a46468f205276282efb7492ad472051134ea1f2e
Rafal Florczak <florczak.raf@gmail.com>
2018-08-19 07:42:07 -1100

    Add staging koan

--- git-koans/conftest.py
+++ git-koans/conftest.py
@@ -54,3 +54,19 @@ def assert_cloned_repo_exists_in_other_directory(koan, basic_upstream):
     koan.assert_commands()
     koan.assert_repo('cloned_repo')
     koan.assert_remotes(relative_path='cloned_repo', expected_remotes={('origin', koan.upstream)})
+
+
+@pytest.fixture
+def assert_index_includes_added_file(koan):
+    yield
+    koan.assert_commands()
+    koan.assert_repo()
+
+    index = set(file for file, _ in koan.get_repo().index.entries.keys())
+    expected_files = {'foo'}
+    extra_files = index.difference(expected_files)
+
+    assert expected_files.issubset(index), ('Expected to find file `foo` in the index. '
+                                            'Current index is {}'.format(index or 'empty'))
+
+    assert not extra_files, f'Unexpected files in the index: {extra_files}'
--- git-koans/git-koans.py
+++ git-koans/git-koans.py
@@ -16,3 +16,13 @@ def test_clone_repository(koan, assert_cloned_repo_exists):
 def test_clone_repository_into_other_directory(koan, assert_cloned_repo_exists_in_other_directory):
     """ Clone repo which address is available in koan.upstream into `cloned_repo` directory"""
     koan.shell('')
+
+
+def test_add_to_index(koan, assert_index_includes_added_file):
+    """ Create a repository and add a file `foo` to the staging area (a.k.a index). Please note that you might need to
+        use multiple commands to keep your solution clean and natural. Starting from the next koan, there
+        will be only one placeholder by default. Feel free to use as many commands as you think you need.
+    """
+    koan.shell('')
+    koan.shell('')
+    koan.shell('')
