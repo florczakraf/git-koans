fb06e7f7bf2987c762de28167468bd53b258b122
Rafal Florczak <florczak.raf@gmail.com>
2018-09-01 07:31:54 -1100

    Don't use global git config

--- enlighten.sh
+++ enlighten.sh
@@ -3,7 +3,12 @@
 set -e
 
 WORKDIR=$(dirname "$0")
+TMPDIR=$(mktemp -d)
+trap "rm -rf ${TMPDIR}" EXIT
+
 export PYTHONPATH="${WORKDIR}:${PYTHONPATH}"
+export GIT_CONFIG_NOSYSTEM=1
+export HOME=${TMPDIR}
 
 python "${WORKDIR}/ensure-env.py"
 pytest -svx --tb=line "${WORKDIR}"/git-koans/git-koans.py "${@}"
